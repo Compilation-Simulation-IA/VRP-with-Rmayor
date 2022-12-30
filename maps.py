49b3f4acda99a086f70e8a91bd17b225f315d800
josue_rdgz <josue.rodriguez.990820@gmail.com>
2022-12-30 13:51:09 -0500

    pintando los nodos en el mapa. Nota: pbf size:50mb

--- maps.py
+++ maps.py
@@ -25,8 +25,10 @@ location = geocoder.geocode(address)
 
 df2 = pd.DataFrame({'Location':
                     ['156A, #107, Playa, La Habana, Cuba',
-                     '38 y 5ta A,Vedado,La Habana,Cuba',
-                     '23 y M, Vedado, La Habana, Cuba']})
+                     '38 y 5ta A,Playa,La Habana,Cuba',
+                     '23 y M, Vedado, La Habana, Cuba',
+                     '23 y G, Vedado, La Habana, Cuba',
+                     'Migas, Calle 17, RadioCuba, El Vedado, Plaza de la Revolución, La Habana, Plaza de la Revolución, 10424, Cuba']})
 
 df2[['location_lat', 'location_long']] = df2['Location'].apply(
     geolocator.geocode).apply(lambda x: pd.Series(
@@ -36,3 +38,42 @@ latitude = location.latitude
 longitude = location.longitude
 print(df2.head())
 print(latitude, longitude)
+
+import osmium
+
+# Carga el archivo PBF en una variable de tipo `osmium.io.Reader`
+reader = osmium.io.Reader('VRP-IVNS/cuba-latest.osm.pbf')
+
+# Crea una variable de tipo `osmium.SimpleHandler` para procesar los datos del archivo PBF
+handler = osmium.SimpleHandler()
+
+# Utiliza el método `apply_file` de `osmium.SimpleHandler` para procesar el archivo PBF
+handler.apply_file('VRP-IVNS/cuba-latest.osm.pbf')
+
+import folium
+
+# Crea un mapa centrado en las coordenadas (latitud, longitud)
+mapa = folium.Map(location=[23.12002,-82.38304],zoom_start=14,zoom_control=False)
+
+# Muestra el mapa en una ventana emergente
+mapa.save('VRP-IVNS/mapa.html')
+
+for i in range (0,len(df2['Location'])):
+    folium.Circle(location=(df2['location_lat'][i], df2['location_long'][i])).add_to(mapa)
+    
+mapa.save('VRP-IVNS/mapa.html')
+# coordenadas = []
+# for node in handler.nodes():
+#     coordenadas.append([node.lat, node.lon])
+
+# # Dibuja las calles del archivo PBF en el mapa como líneas
+# folium.PolyLine(coordenadas, color='red', weight=2.5, opacity=1).add_to(mapa)
+
+# # Recorre las relaciones (polígonos) del archivo PBF y guarda sus propiedades y coordenadas en dos listas
+# propiedades = []
+# coordenadas_poligonos = []
+# for relation in handler.relations():
+#     propiedad = relation.tags.get('name')
+#     propiedades.append(propiedad)
+#     coordenadas_poligono = []
+#     for member
