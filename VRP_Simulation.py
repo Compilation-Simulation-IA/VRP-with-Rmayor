46f53cbdfdf248e69cdbf1888b208bbad21244af
Selleen <97145007+Selleen@users.noreply.github.com>
2023-01-04 13:51:17 -0500

    diaas añadido

--- VRP_Simulation.py
+++ VRP_Simulation.py
@@ -6,125 +6,150 @@ import math
 import networkx as nx
 import time
 
+class WeekDays(Enum):
+    Lunes = 1
+    Martes = 2
+    Miércoles = 3
+    Jueves = 4
+    Viernes = 5
+    Sábado = 6
+    Domingo = 7
+
 class VRP_Simulation:
-    def __init__(self, graph, company: Company):
+    def __init__(self, graph, company: Company, days: int):
         self.graph_map = graph
         self.company = company
         self.global_time = 0
         self.go_back = False
+        self.current_date = 1
+        self.days = days
+        self.week_day = WeekDays.Lunes.name
         
     def simulate_routing(self):
-        while True:
-            end = 0
-            print(f"Tiempo: {self.global_time} min")
-            # Iterar sobre cada uno de los vehículos de la compañía
-            for vehicle in self.company.vehicles:
-                if vehicle.days_off == 0: #obtiene la ruta asignada al vehículo y el nodo actual en el que se encuentra.
-                    actual_route = vehicle.route
-                    current_location = vehicle.current_location
-                    self.print_vehicle_locations(vehicle)
-                    next_node = vehicle.route.next_stop(current_location)
-                    edge = self.graph_map.get_edge_data(current_location, next_node)
-
-                    #El tiempo pasa mientras el vehiculo hace otra funcion
-                    if vehicle.wait > 0:
-                        #en la arista donde se esta moviendo el vehiculo hay una autoridad o semaforo:
-                        if vehicle.state == 1:
-                            if vehicle.pos_traffic_edge >= 0 and len(edge['weight']['traffic_authorities']) > vehicle.pos_traffic_edge:
-                                traffic_a = edge['weight']['traffic_authorities'][vehicle.pos_traffic_edge]
-                                if isinstance(traffic_a, Authority):
-                                    decision = traffic_a.stop_vehicle()
-                                    if decision:
-                                        print(f"El {vehicle} fue parado por la {traffic_a}.")
-                                        self.recalculate_route()
-                                        vehicle.wait = vehicle.pos_traffic_edge + 2 #simula el tiempo q se demoro el vehiculo en encontrarse con la autoridad en la arista. Es el tiempo volviendo para atras
-                                        vehicle.state = 6
-                                    else:
-                                        print(f"El {vehicle} paso la {traffic_a} y no lo pararon.")
-                                else: # isinstance(i, Semaphore)
-                                    color, semaphore_time = traffic_a.get_color(self.global_time)
-                                    if color == Color.YELLOW:
-                                        if vehicle.pass_red():#no hace nada
-                                            print(f"El {vehicle} NO paró en la luz amarilla del {traffic_a}.")
-                                        else:
-                                            new_wait = semaphore_time + traffic_a.color_range[2]
-                                            vehicle.wait += new_wait
-                                            print(f"El {vehicle} le cogio la luz amarilla en el {traffic_a} y paró. Tiempo de espera: {new_wait}.")
-                                            vehicle.state = 5
-
-                                    elif color == Color.RED:
-                                        vehicle.wait += semaphore_time
-                                        print(f"El {vehicle} le cogio la luz roja en el {traffic_a}. Tiempo de espera: {semaphore_time}.")
-                                        vehicle.state = 5  
-
-                        elif vehicle.state == 5:
-                            traffic_a = edge['weight']['traffic_authorities'][vehicle.pos_traffic_edge]
-                            color, semaphore_time = traffic_a.get_color(self.global_time)
-                            if color == Color.GREEN:
-                                vehicle.state = 1
-                                vehicle.pos_traffic_edge += 1
-                        if vehicle.wait - 1 == 0:#ultimo ciclo de espera
-                            if vehicle.state == 1:
-                                next_node = vehicle.route.next_stop(current_location)
-                                cost = self.graph_map.get_edge_data(current_location, next_node)['weight']['weight']
-                                vehicle.move(next_node, cost)
-                                actual_route.actual_stop_index = actual_route.stops.index(next_node)
-                                vehicle.pos_traffic_edge = 0
-                            elif vehicle.state == 5:
-                                vehicle.state = 1
-                                vehicle.pos_traffic_edge += 1
-                            elif vehicle.state == 6:
-                                print(f"Recalculando ruta...")
-                                vehicle.state = 1
-                                vehicle.pos_traffic_edge += 1
-                            vehicle.total_time_wait = 0
-                        vehicle.wait -= 1
-                    # Si el nodo actual es un warehouse, significa que el vehículo va a salir del depósito y comenzar su ruta
-                    # El vehiculo termino de recoger a los clientes de esa parada y se dispone a ir a la siguiente parada
-                    #  El vehiculo esta volviendo y termino de dejar a los clientes de esa parada y se dispone a ir a la siguiente parada
-                    # El vehiculo llega a una parada donde no tiene q recoger a nadie entonces continua (isinstance(current_location, None))
-                    elif (isinstance(current_location, Warehouse) and vehicle.state == 0) or (isinstance(current_location, Stop) and (vehicle.state == 2 or vehicle.state == 3)) or ((not isinstance(current_location, Stop) and not isinstance(current_location, Warehouse)) and vehicle.state == 1):
+        print("Comienza la simulación")
+        while self.current_date <= self.days:
+            print(f"Día {self.current_date}, {self.week_day}")
+            print()
+            self.global_time = 0
+            while True:
+                end = 0
+                print(f"Tiempo: {self.global_time} min")
+                # Iterar sobre cada uno de los vehículos de la compañía
+                for vehicle in self.company.vehicles:
+                    if vehicle.days_off == 0: #obtiene la ruta asignada al vehículo y el nodo actual en el que se encuentra.
+                        actual_route = vehicle.route
+                        current_location = vehicle.current_location
+                        self.print_vehicle_locations(vehicle)
                         next_node = vehicle.route.next_stop(current_location)
                         edge = self.graph_map.get_edge_data(current_location, next_node)
-                        vehicle.wait = vehicle.total_time_wait = edge['weight']['weight']
-                        vehicle.state = 1
-                    #Si el nodo actual es una parada de tipo Stop, significa que el vehículo debe recoger a los clientes que se encuentran en esa parada
-                    elif isinstance(current_location, Stop) and vehicle.state == 1:
-                        if self.go_back:
+
+                        #El tiempo pasa mientras el vehiculo hace otra funcion
+                        if vehicle.wait > 0:
+                            #en la arista donde se esta moviendo el vehiculo hay una autoridad o semaforo:
+                            if vehicle.state == 1:
+                                if vehicle.pos_traffic_edge >= 0 and len(edge['weight']['traffic_authorities']) > vehicle.pos_traffic_edge:
+                                    traffic_a = edge['weight']['traffic_authorities'][vehicle.pos_traffic_edge]
+                                    if isinstance(traffic_a, Authority):
+                                        decision = traffic_a.stop_vehicle()
+                                        if decision:
+                                            print(f"El {vehicle} fue parado por la {traffic_a}.")
+                                            self.recalculate_route()
+                                            vehicle.wait = vehicle.pos_traffic_edge + 2 #simula el tiempo q se demoro el vehiculo en encontrarse con la autoridad en la arista. Es el tiempo volviendo para atras
+                                            vehicle.state = 6
+                                        else:
+                                            print(f"El {vehicle} paso la {traffic_a} y no lo pararon.")
+                                    else: # isinstance(i, Semaphore)
+                                        color, semaphore_time = traffic_a.get_color(self.global_time)
+                                        if color == Color.YELLOW:
+                                            if vehicle.pass_red():#no hace nada
+                                                print(f"El {vehicle} NO paró en la luz amarilla del {traffic_a}.")
+                                                vehicle.pos_traffic_edge += 1
+                                            else:
+                                                new_wait = semaphore_time + traffic_a.color_range[2]
+                                                vehicle.wait += new_wait
+                                                print(f"El {vehicle} le cogio la luz amarilla en el {traffic_a} y paró. Tiempo de espera: {new_wait}.")
+                                                vehicle.state = 5
+
+                                        elif color == Color.RED:
+                                            vehicle.wait += semaphore_time
+                                            print(f"El {vehicle} le cogio la luz roja en el {traffic_a}. Tiempo de espera: {semaphore_time}.")
+                                            vehicle.state = 5  
+
+                            elif vehicle.state == 5:
+                                traffic_a = edge['weight']['traffic_authorities'][vehicle.pos_traffic_edge]
+                                color, semaphore_time = traffic_a.get_color(self.global_time)
+                                if color == Color.GREEN:
+                                    vehicle.state = 1
+                                    vehicle.pos_traffic_edge += 1
+                            if vehicle.wait - 1 == 0:#ultimo ciclo de espera
+                                if vehicle.state == 1:
+                                    next_node = vehicle.route.next_stop(current_location)
+                                    cost = self.graph_map.get_edge_data(current_location, next_node)['weight']['weight']
+                                    vehicle.move(next_node, cost)
+                                    actual_route.actual_stop_index = actual_route.stops.index(next_node)
+                                    vehicle.pos_traffic_edge = 0
+                                elif vehicle.state == 5:
+                                    vehicle.state = 1
+                                    vehicle.pos_traffic_edge += 1
+                                elif vehicle.state == 6:
+                                    print(f"Recalculando ruta...")
+                                    vehicle.state = 1
+                                    vehicle.pos_traffic_edge += 1
+                                vehicle.total_time_wait = 0
+                            vehicle.wait -= 1
+                        # Si el nodo actual es un warehouse, significa que el vehículo va a salir del depósito y comenzar su ruta
+                        # El vehiculo termino de recoger a los clientes de esa parada y se dispone a ir a la siguiente parada
+                        #  El vehiculo esta volviendo y termino de dejar a los clientes de esa parada y se dispone a ir a la siguiente parada
+                        # El vehiculo llega a una parada donde no tiene q recoger a nadie entonces continua (isinstance(current_location, None))
+                        elif (isinstance(current_location, Warehouse) and vehicle.state == 0) or (isinstance(current_location, Stop) and (vehicle.state == 2 or vehicle.state == 3)) or ((not isinstance(current_location, Stop) and not isinstance(current_location, Warehouse)) and vehicle.state == 1):
+                            next_node = vehicle.route.next_stop(current_location)
+                            edge = self.graph_map.get_edge_data(current_location, next_node)
+                            vehicle.wait = vehicle.total_time_wait = edge['weight']['weight']
+                            vehicle.state = 1
+                        #Si el nodo actual es una parada de tipo Stop, significa que el vehículo debe recoger a los clientes que se encuentran en esa parada
+                        elif isinstance(current_location, Stop) and vehicle.state == 1:
+                            if self.go_back:
+                                off = vehicle.drop_off_clients()
+                                vehicle.wait = vehicle.total_time_wait = off #tiempo que se demora cada persona en bajar
+                                vehicle.state = 3
+                            else:
+                                on_board = vehicle.pick_up_clients()
+                                vehicle.wait = vehicle.total_time_wait = on_board # cada persona se demora 3 min en subir al carro
+                                vehicle.state = 2
+                        # Si el nodo actual es el ultimo de la ruta entonces se bajan los clientes
+                        # O si esta volviendo el vehiculo, va dejando a los clientes en sus respectivas paradas
+                        elif actual_route.actual_stop_index == len(actual_route.stops) - 1 and vehicle.state == 1 and not self.go_back:  
                             off = vehicle.drop_off_clients()
                             vehicle.wait = vehicle.total_time_wait = off #tiempo que se demora cada persona en bajar
                             vehicle.state = 3
-                        else:
-                            on_board = vehicle.pick_up_clients()
-                            vehicle.wait = vehicle.total_time_wait = on_board # cada persona se demora 3 min en subir al carro
-                            vehicle.state = 2
-                    # Si el nodo actual es el ultimo de la ruta entonces se bajan los clientes
-                    # O si esta volviendo el vehiculo, va dejando a los clientes en sus respectivas paradas
-                    elif actual_route.actual_stop_index == len(actual_route.stops) - 1 and vehicle.state == 1 and not self.go_back:  
-                        off = vehicle.drop_off_clients()
-                        vehicle.wait = vehicle.total_time_wait = off #tiempo que se demora cada persona en bajar
-                        vehicle.state = 3
-                    # El vehiculo termino la ruta => comprobar su estado para mandarlo a mantenimiento
-                    elif end != len(self.company.vehicles) and ((vehicle.state == 3 and not self.go_back) or (vehicle.state == 1 and self.go_back) ):
-                        company.check_vehicle(vehicle)
-                        end += 1
+                        # El vehiculo termino la ruta => comprobar su estado para mandarlo a mantenimiento
+                        elif end != len(self.company.vehicles) and ((vehicle.state == 3 and not self.go_back) or (vehicle.state == 1 and self.go_back) ):
+                            company.check_vehicle(vehicle)
+                            end += 1
 
-                else:
-                    #El vehiculo esta en mantenimiento y en un warehouse
-                    if vehicle.wait - 1 == 0 and vehicle.state == 4:
-                        vehicle.days_off = 0
-                        company.check_vehicle(vehicle)
-                        vehicle.total_time_wait = vehicle.state = 0
-                    vehicle.wait -= 1
+                    else:
+                        #El vehiculo esta en mantenimiento y en un warehouse
+                        if vehicle.wait - 1 == 0 and vehicle.state == 4:
+                            vehicle.days_off = 0
+                            company.check_vehicle(vehicle)
+                            vehicle.total_time_wait = vehicle.state = 0
+                        vehicle.wait -= 1
 
-            if end == len(self.company.vehicles):
-                print(f"Todos los vehiculos concluyeron sus respectivas rutas.")
-                print(f"Pasando el tiempo...")
-                time.sleep(5)
-                self.reorder_rutes()
-                self.go_back = True
-                end = 0
-            self.global_time +=1
+                if end == len(self.company.vehicles):
+                    print(f"Todos los vehículos concluyeron sus respectivas rutas.")
+                    if not self.go_back:
+                        print(f"Pasando el tiempo...")
+                        time.sleep(5)
+                        self.reorder_rutes()
+                        self.go_back = True
+                        end = 0
+                self.global_time +=1
+                time.sleep(1)
+                if self.go_back and end == len(self.company.vehicles): #Termino la ruta de vuelta
+                    print(f"Terminaron los recorridos del Día {self.current_date}, {self.week_day}")
+                    break
+            self.current_date +=1
+            self.week_day = WeekDays(self.current_date % 7).name
             time.sleep(1)
         
     #vuelve al nodo anterior(siempre estuvo alli), recalcula la ruta(cambiar los costos de las aristas),
@@ -153,8 +178,9 @@ class VRP_Simulation:
                     new_vehicle = self.generate_vehicle()
                     self.company.buy_vehicle(new_vehicle, 100)
             on_board = 0
-            for stop in vehicle.route:
-                on_board += stop.total_client
+            for stop in vehicle.route.stops:
+                if isinstance(stop, Stop):
+                    on_board += stop.total_client
             vehicle.clients_on_board = on_board
 
     def print_vehicle_locations(self, vehicle: Vehicle):
@@ -254,7 +280,7 @@ vehicle1.assign_route(route1)
 vehicle2.assign_route(route2)
 
 # Inicializar la compañía
-company = Company("Compañía de Transporte", 10000)  # Inicializar con un presupuesto de 10000 pesos
+company = Company("Compañía de Transporte", 100000)  # Inicializar con un presupuesto de 10000 pesos
 
 # Añadir los warehouses a la compañía
 company.add_warehouse(nodos[8])
@@ -269,7 +295,7 @@ company.buy_vehicle(vehicle2, 300)
 company.add_route(route1)
 company.add_route(route2)
 
-vrp = VRP_Simulation(graph, company)
+vrp = VRP_Simulation(graph, company, 3)
 vrp.simulate_routing()
 print("exito")
 
